b48472d187ab88f4972826f772a42b1f67de8770
Wh1isper <43375501+Wh1isper@users.noreply.github.com>
2019-09-27 21:02:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ MAX_STEP = 150
 
 
 def recur_solotion(state):
+    # 猴子拿香蕉4步走：
+    # 1. 去箱子（箱子没就位）
+    # 2. 搬箱子到香蕉（猴子就位）
+    # 3. 站上箱子（箱子就位 猴子就位）
+    # 4. 拿到香蕉
+    # 然后我们反着看这个过程，就是这个函数的书写顺序了
+    # 这个函数将从4到1检查某件事是否可行，满足需求则做
     if len(state) > MAX_STEP:
         print("Can't reach")
         exit(-1)
@@ -33,4 +40,8 @@ def recur_solotion(state):
 
 
 if __name__ == '__main__':
-    recur_solotion(State(monkey="赵家", box="钱家", banana="孙家"))
+    monkey = input("猴子猴子在哪里？")
+    box = input("箱子箱子在哪里？")
+    banana = input("香蕉香蕉在哪里？")
+    # 你不输入的话，猴子箱子香蕉就在一起啦！
+    recur_solotion(State(monkey=monkey, box=box, banana=banana))
